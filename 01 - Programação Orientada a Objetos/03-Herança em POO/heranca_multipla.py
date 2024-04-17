879f5b8f2909f046251a512d27c28308a3345e69
André Dimitrin <82331544+andredimitrin@users.noreply.github.com>
2024-04-17 20:42:16 -0300

    att

--- 01 - Programação Orientada a Objetos/03-Herança em POO/heranca_multipla.py
+++ 01 - Programação Orientada a Objetos/03-Herança em POO/heranca_multipla.py
@@ -1,20 +1,29 @@
 class Animal:
-    pass
+    def __init__(self, nro_patas):
+        self.nro_patas = nro_patas
+        
+    def __str__(self):
+        return f"{self.__class__.__name__}:{', '.join([f'{chave}={valor}'
+    for chave, valor in self.__dict__.items()])}"
 
 class Mamifero(Animal):
-    pass
+    def __init__(self, nro_patas):
+        self.nro_patas = nro_patas
+        super().__init__(nro_patas)
 
 class Ave(Animal):
-    pass    
+    def __init__(self, nro_patas):
+        self.nro_patas = nro_patas
+        super().__init__(nro_patas) 
 
-class Cachorro(Mamifero):
-    pass
 
 class Gato(Mamifero):
     pass
 
-class Leao(Mamifero):
-    pass
 
 class Ornitorrinco(Mamifero, Ave):
     pass
+
+
+gato = Gato(4)
+print(gato)
